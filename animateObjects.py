ca819308591de612d474df60640af7233472c64b
tesseradecades <nwe8978@rit.edu>
2018-04-29 13:30:59 -0400

    input and output for attack damage

--- animateObjects.py
+++ animateObjects.py
@@ -1,6 +1,12 @@
 import random
 from enum import Enum
 
+"""
+Python script to ease damage rolls for the animate objects spell. Rolling 10d20 
+for hits was slightly annoying...then our paladin knocked the enemy prone and
+we had to roll 10d20 with advantage for hits.
+"""
+
 class Advantage(Enum):
     disadvantage = -1
     none = 0
@@ -11,7 +17,7 @@ def xdy(x: int, y: int) -> list:
 
 def xdyplus(x: int, y: int, modifier: int) -> list:
     rolls = list()
-    for i in range(1,x)
+    for i in range(0,x):
         rolls.append(random.randint(1,y)+modifier)
     return rolls
 
@@ -21,7 +27,7 @@ def attackRoll(attackBonus: int, advantage: Advantage, critFloor: int) -> tuple:
         roll = rolls[1]
     else:
         roll = rolls[0]
-    return tuple(roll+attackBonus,withinCritRange(critFloor,roll))
+    return (roll+attackBonus,withinCritRange(critFloor,roll))
 
 def withinCritRange(critFloor: int, roll: int) -> bool:
     return critFloor <= roll
@@ -43,7 +49,39 @@ def hugeAttack(advantage: Advantage) -> tuple:
 
 def attack(advantage: Advantage, attackModifier: int, damageModifier: int, damageDie: int, damageRolls: int) -> tuple:
     atkRoll = attackRoll(attackModifier,advantage,20)
-    damage = xdyplus(damageRolls,damageDie,damageModifier)
+    damage = 0
+    for dmg in xdyplus(damageRolls,damageDie,damageModifier):
+        damage+=dmg
     if(atkRoll[1]):
-        damage+=xdy(1,damageDie)
-    return tuple(atkRoll,damage)
+        damage+=random.randint(1,damageDie)#xdy(1,damageDie)
+    return (atkRoll[0],damage)
+
+def main():
+    totalAttacks = list()
+    print("How many tiny attacks?")
+    tinyAttacks = int(input())
+    for x in range(0,tinyAttacks):
+        attack = tinyAttack(Advantage.none)
+        totalAttacks.append(attack)
+        print(attack)
+    
+    #small attacks
+    print("How many small attacks?")
+    smallAttacks = int(input())
+    for x in range(0,smallAttacks):
+        attack = smallAttack(Advantage.none)
+        totalAttacks.append(attack)
+        print(attack)
+    print("What is the target AC?")
+    targetAC = int(input())
+    totalHits = 0
+    totalDamage = 0
+    for x in totalAttacks:
+        if(x[0] >= targetAC):
+            totalHits+=1
+            totalDamage+=x[1]
+    print("Total Hits:\t",totalHits)
+    print("Total Damage:\t",totalDamage)
+
+if __name__ == "__main__":
+    main()
